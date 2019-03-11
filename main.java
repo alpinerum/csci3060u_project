4eba043d5e03cd566fbfb13a187538f819ad6946
ammarkhan310 <ammarkhan310@gmail.com>
2019-03-10 22:21:14 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+/**
+ * Main class which handles all the methods
+ */
+public class main {
+
+  public static void main(String[] args) {
+      TODO: //handle methods
+    }
+  }
+}
