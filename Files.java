b0f6f1f7325c330c84a27efab29cca76a9734f35
jackie0448 <youcun555@gmail.com>
2019-03-11 10:00:49 +0800

    Add files via upload

--- Files.java
+++ Files.java
@@ -7,12 +7,27 @@ public class Files{
          * Reads the Daily transaction file
          */
 
-        public static void readFile() throws IOException{
+        //public static void readFile() throws IOException{
             //TODO: Read file
-        }
-
-        public mergeTransFiles(){
+        //}
+    	File transFile = new File();//getting the directory where the daily transaction files are
+    	File[] fileSources = new File();
+    	File outputFile= new File();
+    	int transCode;
+    	
+    	String inputFileName = new String (); //string of the file to that needs to be checked for negative value
+        
+    	
+    	public mergeTransFiles(File outputFile, File[] fileSources) throws IOException{
             //TODO: Merge transaction files
         }
+        public negativeTixCheck(int transacCode, String inputFileName) {
+        	//TODO: check for negative values of tickets left
+        }
+        
+        public String[] listOfTransacFile ( File transacFileDirectory){
+        	//TODOL returns list of transaction files in the directory it the form of String[]
+        }
+        
 
     }
