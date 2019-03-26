51f539a6b9518d90e3a3b227478e333253aed77a
jackie0448 <youcun555@gmail.com>
2019-03-26 10:17:08 -0400

    Update Files1.java
    moved the getCorrespondingLineInFile() method over from tix class

--- Files1.java
+++ Files1.java
@@ -1,3 +1,4 @@
+    
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,12 +6,14 @@
  */
 package phase4;
 import java.io.*;
+import java.util.ArrayList;
 /**
  *
  * @author YouCun
  */
 public class Files1 {
     public void mergeTransFiles (String dirName, String outputFile) throws IOException{
+        // this method merges all of the transaction files in the same directory into 1 file
         File dir = new File(dirName); //create instance of directory
         PrintWriter pw = new PrintWriter (outputFile); //create obj for printwriter for output file
         String[] fileNamesInDir = dir.list(); //putting names of all trans file into an arraylist
@@ -31,5 +34,19 @@ public class Files1 {
         System.out.println("Reading from all files" +  
         " in directory " + dir.getName() + " Completed"); 
         
+    }
+    
+    public String getCorrespondingLineInFile(String userName, ArrayList<String> parseList){
+        //this method returns the entry in the parsed list, eg tix or accounts that belongs to the user.
+           String lineInParseFile;
+           for (int i = 0; i < parseList.size(); i ++){
+               lineInParseFile= parseList.get(i);
+               if ( lineInParseFile.contains(userName)){
+                   return lineInParseFile;
+               }
+              
+        }
+           System.out.println(" No such entry in the list!");
+           return null;
     }
 }
